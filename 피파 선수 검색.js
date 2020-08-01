c0c302bd6cf706544990f268d3dcb8096bdf86f5
Hott-J <jhj13062004@naver.com>
2020-08-01 21:57:57 +0900

    피파 Addict 선수 검색

--- 피파 선수 검색.js
+++ 피파 선수 검색.js
@@ -21,8 +21,8 @@ const Kakao=new kalingModule;
 
 function kakao_login(replier){
   try{
-    Kakao.init('2d0e4efc34993dc5353ebb2f964f3f4d'); // 중요포인트 : 반드시 봇계정 카카오아이디와 패스워드로 카카오디벨로퍼에 로그인하여 자바스크립트 키값을 받아올것!
-    Kakao.login('jhj07152019@gmail.com','rkdmfdl.7');//중요포인트 : 반드시 봇계정 카카오아이디와 패스워드를 적어줄것!!
+    Kakao.init('myJsId'); // 중요포인트 : 반드시 봇계정 카카오아이디와 패스워드로 카카오디벨로퍼에 로그인하여 자바스크립트 키값을 받아올것!
+    Kakao.login('myKakaoId','password');//중요포인트 : 반드시 봇계정 카카오아이디와 패스워드를 적어줄것!!
     doc = Jsoup.connect("https://kr.fifaaddict.com/fo4db?playername="+search).get();
   }catch(e){replier.reply("로그인 세션이 만료되었습니다.")}
 }
@@ -87,7 +87,7 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
       l5:id[4].split('.com/')[1]
     }
     if(flag){
-      send_template(room,33609,set)
+      send_template(room,templateId,set)
     }
     else{
       replier.reply("선수 이름을 다시 확인해주세요.")
